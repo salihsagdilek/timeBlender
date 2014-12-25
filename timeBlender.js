0181a08ff67d809c4d39fc0cbe2ae1fffc20101b
salihsagdilek <mail@salihsagdilek.com>
2014-12-25 04:16:05 +0200

    js update

--- timeBlender.js
+++ timeBlender.js
@@ -4,14 +4,18 @@
 (function refreshData(){
     var x = 1,
         d = new Date(),
-        h = d.getHours(),
-        m = d.getMinutes(),
-        s = d.getSeconds();
-    h <= 9 ? h = '0' + h : '';
-    m <= 9 ? m = '0' + m : '';
-    s <= 9 ? s = '0' + s : '';
+        h = d.getHours() <= 9 ? '0' + d.getHours() : d.getHours(),
+        m = d.getMinutes() <= 9 ? '0' + d.getMinutes() : d.getMinutes(),
+        s = d.getSeconds() <= 9 ? '0' + d.getSeconds() : d.getSeconds(),
+        alpha = '.7',
+        rgba = 'rgba(' + parseInt((parseInt(m,16)*1.2) + 25) + ',' + (parseInt(h,16)) + ',' + parseInt((parseInt(s,16)*1.7) + 100).toString() + ',' + alpha + ')';
 
-    document.getElementsByTagName('body')[0].style.backgroundColor = '#'+h+m+s;
+    var content = document.getElementById('full');
+    content.style.backgroundImage = 'url(https://vanillatours.com/Content/pictures/search/full-width-' + (parseInt(d.getMinutes() / 2) == 0 ? 1 : parseInt(d.getMinutes() / 2))+ '.jpg)';
+    content.style.backgroundSize = 'cover';
+    content.style.backgroundPosition = 'center';
+    content.style.backgroundColor = rgba;
+    content.style.backgroundBlendMode = 'screen';
     document.getElementById('hour').innerText = h;
     document.getElementById('minute').innerText = m;
     document.getElementById('second').innerText = s;
